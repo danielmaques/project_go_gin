abad748e8b7949706a0cab9c1ea4cb75f4a6ad8f
Daniel Marques <ddanielddiniz@gmail.com>
2023-04-22 22:31:51 -0300

    ConectaBancoDeDados

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/danielmaques/go_gin/database"
 	"github.com/danielmaques/go_gin/models"
 	"github.com/danielmaques/go_gin/routes"
 )
 
 func main(){
+	database.ConectaBancoDeDados()
 	models.Alunos = []models.Aluno{
 		{Nome: "Julho", CPF: "390.407.090-84"},
 		{Nome: "Ana", CPF: "014.051.990-40"},
